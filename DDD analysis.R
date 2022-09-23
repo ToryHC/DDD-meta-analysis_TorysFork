79f2b47a8d33ab68f4dfe4aa7e9a9c290387d14f
Nathalie Jreidini <nathaliejreidini@Nats-MacBook-Pro.local>
2022-09-23 18:34:20 -0400

    -

---
+++ DDD analysis.R
@@ -0,0 +1,5 @@
+## Author: Nathalie Jreidini
+## Purpose: for analysis of density-dependent dispersal data for LDP project
+## download csv from OSF
+data <- osfr::osf_retrieve_node("https://osf.io/vfytd")
+## NOTE: not using groundhog so as not to interfere with alredy existing R code.
